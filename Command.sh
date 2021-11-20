1399813ed834d3bcc6b7e6aa4c2bf08bc305ba12
YimingZackZhang <93574256+YimingZackZhang@users.noreply.github.com>
2021-11-20 00:27:06 -0500

    Update Command.sh

--- Command.sh
+++ Command.sh
@@ -1,1 +1,1 @@
-./parMix -c 3 -p 1 -b 259000000 -P example/testposition.dat -C example/testgenotypes.dat -A example/testAFA.dat -B example/testAFB.dat -D example/testLDA.dat -E example/testLDB.dat -R 0.00000001 -r 0.000002
+./parMix -c 3 -p 2 -b 259000000 -P example/testposition.dat -C example/testgenotypes.dat -A example/testAFA.dat -B example/testAFB.dat -D example/testLDA.dat -E example/testLDB.dat -R 0.00000001 -r 0.000002
