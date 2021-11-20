4cce3ab44d9cf2bcc9d35c8e4ee6ca17169cc74e
YimingZackZhang <93574256+YimingZackZhang@users.noreply.github.com>
2021-11-19 21:37:20 -0500

    Delete Command.sh

--- Command.sh
+++
@@ -1,1 +0,0 @@
-python3 parMix.py -c 3 -p 2 -b 259000000 -P example/testposition.dat -C example/testgenotypes.dat -A example/testAFA.dat -B example/testAFB.dat -D example/testLDA.dat -E example/testLDB.dat -R 0.00000001 -r 0.000002
